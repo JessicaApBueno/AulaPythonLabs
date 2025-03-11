442f0094a1e0478809f97ac24d91da9ec80072c2
JessicaApBueno <buenojessicaaparecida@gmail.com>
2025-03-11 19:31:46 -0300

    update

--- exercicio5.py
+++ exercicio5.py
@@ -1,18 +1,18 @@
 def registrar_notas():
     notas = []
     while True:
-            try:
-                entrada = float(input("Digite a nota do aluno (ou 'fim' para encerrar): "))
-                if entrada.lower() == 'fim':
-                   break                
-                nota = float(entrada)
-                if 0 <= nota <= 10:
-                    notas.append(nota)
-                else:
-                    print("Nota inválida. Digite um valor de 0 a 10.")            
-            except ValueError:
-                print('Entrada inválida. Por favor, digite um número válido')
-            
+        entrada = input("Digite a nota do aluno (ou 'fim' para encerrar): ")
+        if entrada.lower() == 'fim':
+            break
+        try:
+            nota = float(entrada)
+            if 0 <= nota <= 10:
+                notas.append(nota)
+            else:
+                print("Nota inválida. Digite um valor de 0 a 10.")
+        except ValueError:
+            print('Entrada inválida. Por favor, digite um número válido')
+
     if notas:
         media = sum(notas) / len(notas)
         print(f"\nMédia da turma: {media:.2f}")
@@ -21,4 +21,6 @@ def registrar_notas():
         print("\nNenhuma nota válida foi registrada.")
 
 registrar_notas()
+
+
 
