3b9d94e005d78dfdef02c3cd540eaaf3d22c6e60
Jessica  Bueno <158577161+JessicaApBueno@users.noreply.github.com>
2025-03-11 23:16:18 -0300

    Update aula1.py

--- aula1.py
+++ aula1.py
@@ -7,26 +7,11 @@ fruta_citrica_maio = "melancia"
 #Imprimindo a variável
 print(fruta_citrica_maio)
 
-#Expressões Matemáticas Python
+fruta = "melão"
+print(fruta)
 
-print (2*3+3**2)
+#Expressões matemáticas
+print(2*3+3*2)
 
 carro = "fox"
 print(carro)
-
-# Função para adicionar uma pessoa á lista
-def adicionar_pessoa(lista, nome, idade, profissao):
-    pessoa = {"nome": nome, "idade": idade, "profissao": profissao}
-    lista.append(pessoa)
-
-#Função para mostrar as pessoas da lista
-
-def exibir_pessoas(lista):
-    print("Lista de pessoas cadastradas")
-    for pessoa in lista:
-        print(f"Nome: {pessoa['nome']}, Idade: {pessoa['idade']}, Profissao: {pessoa['profissao']}")
-
-# Lista para armazenar pessoas
-pessoas = []
-
-# Adicionando
